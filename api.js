1877cd4011e9cbc070d2610d4bdb09c857255063
Dhruv <dhruvgoel165@gmail.com>
2025-02-25 17:01:28 +0530

    minor update

--- api.js
+++ api.js
@@ -18,10 +18,7 @@ const saltRounds = 10;
 
 console.log(process.env.MONGO_URI)
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(MONGO_URI, { dbName: "markify" });
 
 const userSchema = new mongoose.Schema({
   name: String,
