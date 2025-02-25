ac0ed85901e5447bb50d3e20a79920694a459d98
Dhruv <dhruvgoel165@gmail.com>
2025-02-25 18:06:47 +0530

    mongodb

--- api.js
+++ api.js
@@ -16,7 +16,10 @@ const port = 3000;
 dotenv.config();
 const saltRounds = 10;
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { dbName: "markify" });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const userSchema = new mongoose.Schema({
   name: String,
